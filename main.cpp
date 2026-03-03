1be91585fed90598a9324697577a6c47dc9757f9
udayshinde75 <dadashinde247@gmailcom>
2026-03-03 22:39:42 +0530

    From now on wil code through different machines, so will push small code snippets whenever i get time.
    Added shader helpers for next program.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,36 @@
-// Will learn something new here
+// Adding attenuation (dimming light)
+
+#include <iostream>
+#include "glad/glad.h"
+using namespace std;
+// Shader helpers : ----
+int success;
+char infoLog[512];
+
+GLuint compileShader(GLenum type, const GLchar* source) {
+    const GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, nullptr);
+    glCompileShader(shader);
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader, 512, nullptr, infoLog);
+        cout << "Error compiling shader: " << infoLog << endl;
+        return 0;
+    }
+    return shader;
+}
+
+GLuint createProgram(const char* vertexSource, const char* fragmentSource) {
+    GLuint program = glCreateProgram();
+    GLuint vertexShader = compileShader(GL_VERTEX_SHADER, vertexSource);
+    GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentSource);
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(program, 512, nullptr, infoLog);
+        cout << "Error linking program: " << infoLog << endl;
+    }
+    return  program;
+}
